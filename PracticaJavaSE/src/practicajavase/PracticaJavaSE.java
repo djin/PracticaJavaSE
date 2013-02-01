9fbcb670302090c01025e9dd79e96541e4a72c4e
Carlos Gomez <superdjin@gmail.com>
2013-02-01 16:53:25 +0100

    Config repo

---
+++ PracticaJavaSE/src/practicajavase/PracticaJavaSE.java
@@ -0,0 +1,15 @@
+package practicajavase;
+
+/**
+ *
+ * @author 66785379
+ */
+public class PracticaJavaSE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
